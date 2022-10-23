dfd263b421e2b4f924e5d847a59bc141573d247d
Anderson <andemartinezdev@gmail.com>
2022-10-23 11:08:11 -0500

    31 - From sentense
    - We played with some forms to obtain the data.

--- platziblog/select.sql
+++ platziblog/select.sql
@@ -25,3 +25,50 @@ FROM posts;
 
 SELECT COUNT(*) AS total_posts
 FROM posts;
+
+-- 31. Sentencia FROM
+
+-- left join
+SELECT *
+FROM usuarios
+LEFT JOIN posts ON usuarios.id = posts.usuario_id;
+
+SELECT *
+FROM usuarios
+LEFT JOIN posts ON usuarios.id = posts.usuario_id
+WHERE posts.usuario_id IS NULL;
+
+-- Right join
+SELECT *
+FROM usuarios
+RIGHT JOIN posts ON usuarios.id = posts.usuario_id;
+
+SELECT *
+FROM usuarios
+RIGHT JOIN posts ON usuarios.id = posts.usuario_id
+WHERE posts.usuario_id IS NULL;
+
+-- Inner Join
+SELECT *
+FROM usuarios
+INNER JOIN posts ON usuarios.id = posts.usuario_id;
+
+-- Union
+SELECT *
+FROM usuarios
+LEFT JOIN posts ON usuarios.id = posts.usuario_id
+UNION
+SELECT *
+FROM usuarios
+RIGHT JOIN posts ON usuarios.id = posts.usuario_id;
+
+-- Simetric diference
+SELECT *
+FROM usuarios
+LEFT JOIN posts ON usuarios.id = posts.usuario_id
+WHERE posts.usuario_id IS NULL
+UNION
+SELECT *
+FROM usuarios
+RIGHT JOIN posts ON usuarios.id = posts.usuario_id
+WHERE posts.usuario_id IS NULL;
