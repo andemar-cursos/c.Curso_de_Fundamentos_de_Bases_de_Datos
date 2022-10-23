73f9df30ed2eb9746d4b34be623f87b56a3da38f
Anderson <andemartinezdev@gmail.com>
2022-10-23 10:07:01 -0500

    29 - SELECT
    - Was insert the values into the tables to do some select to practices.

--- platziblog/select.sql
+++ platziblog/select.sql
@@ -9,3 +9,19 @@ HAVING total >= 2;
 SELECT * 
 FROM posts
 WHERE fecha_publicacion > '2024';
+
+-- 29. SELECT
+SELECT *
+FROM posts;
+
+
+SELECT titulo, fecha_publicacion, estatus
+FROM posts;
+
+
+SELECT titulo AS encabezado, fecha_publicacion AS publicado_en, estatus AS estado
+FROM posts;
+
+
+SELECT COUNT(*) AS total_posts
+FROM posts;
