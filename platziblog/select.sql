a4c8bd9bf086fb1d37560f848f3ee48ad92a8e32
Anderson <andemartinezdev@gmail.com>
2022-10-24 23:18:37 -0500

    34 - Group By
    - In this video we check some forms to group the data with GROUP BY and COUNT(*), allow see some data group by date or value (estatus).

--- platziblog/select.sql
+++ platziblog/select.sql
@@ -161,3 +161,22 @@ WHERE usuario_id IS NOT NULL
     AND id < 50
     AND categoria_id = 2
     AND YEAR(fecha_publicacion) = '2025';
+
+
+
+-- 34. GROUP BY
+SELECT estatus, COUNT(*) AS post_quantity
+FROM posts
+GROUP BY estatus;
+
+SELECT YEAR(fecha_publicacion) AS post_year, COUNT(*) AS post_quantity
+FROM posts
+GROUP BY post_year;
+
+SELECT MONTHNAME(fecha_publicacion) AS post_month, COUNT(*) AS post_quantity
+FROM posts
+GROUP BY post_month;
+
+SELECT estatus, MONTHNAME(fecha_publicacion) AS post_month, COUNT(*) AS post_quantity
+FROM posts
+GROUP BY estatus, post_month;
