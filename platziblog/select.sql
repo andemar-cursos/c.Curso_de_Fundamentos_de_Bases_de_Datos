b3713b3ce0fd02be7039cdf9352cd115a71dc138
Anderson <andemartinezdev@gmail.com>
2022-10-25 21:18:44 -0500

    35 - Order By & having
    - We used new commands to try to organize the data that show in result Query.
    
    Order: Works to order the result data, we can use ASC or DESC to organize the result.
    LIMIT: With this command you can limit the number of row to return.
    HAVING: This works only with group values, but not with column values, this have the same characteristic that where.

--- platziblog/select.sql
+++ platziblog/select.sql
@@ -180,3 +180,31 @@ GROUP BY post_month;
 SELECT estatus, MONTHNAME(fecha_publicacion) AS post_month, COUNT(*) AS post_quantity
 FROM posts
 GROUP BY estatus, post_month;
+
+-- 35. Order by & having
+SELECT *
+FROM posts
+ORDER BY fecha_publicacion ASC;
+
+SELECT *
+FROM posts
+ORDER BY fecha_publicacion DESC;
+
+SELECT *
+FROM posts
+ORDER BY titulo ASC;
+
+SELECT *
+FROM posts
+ORDER BY usuario_id ASC;
+
+SELECT *
+FROM posts
+ORDER BY fecha_publicacion ASC
+LIMIT 5;
+
+SELECT MONTHNAME(fecha_publicacion) AS post_month, estatus, COUNT(*) AS post_quantity
+FROM posts
+GROUP BY estatus, post_month
+HAVING post_quantity > 1
+ORDER BY post_month;
