6651a2fc5f544466f26025ec04b3d5058ca82fab
Anderson <andemartinezdev@gmail.com>
2022-10-25 21:36:21 -0500

    36 - Nested queries
    - We have two examples about nested queries that allow get some specific data that we can't obtain through actual table.

--- platziblog/select.sql
+++ platziblog/select.sql
@@ -208,3 +208,21 @@ FROM posts
 GROUP BY estatus, post_month
 HAVING post_quantity > 1
 ORDER BY post_month;
+
+-- 36. Nested queries
+SELECT new_table_projection.date, COUNT(*) AS posts_count
+FROM (
+    SELECT DATE(MIN(fecha_publicacion)) AS date, YEAR(fecha_publicacion) AS post_year
+    FROM posts
+    GROUP BY post_year
+     ) AS new_table_projection
+GROUP BY new_table_projection.date
+ORDER BY date ASC;
+
+
+SELECT *
+FROM posts
+WHERE fecha_publicacion = (
+    SELECT MAX(fecha_publicacion)
+    FROM posts
+    )
